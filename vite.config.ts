ad03f946bb11065124472cb4842ecbe4503a72cc
flycner01 <flycner01@gmail.com>
2025-09-03 18:02:29 +0900

    🗑️ GitHub Actions 제거: Vercel 전용 배포로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/mykneeboard/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
